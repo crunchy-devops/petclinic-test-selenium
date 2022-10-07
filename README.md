ee351f424ca73f962d9a3b9e3647bf83334ca940
Herve Meftah <dockerlite@gmail.com>
2022-10-07 06:17:22 +0200

    First initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# Set up selenium Tests
+Check the pom.xml definition and the code source of the class day1 
+**Attention** The IP address of the test version of the website is hardcoded that shouldn't 
+be done in the real CI/CD environment.  
+  
+## Create a Jenkins Job
+### Define 
+if the plugin Log Parser is not present  
+Go to manage-Jenkins -> Manage plugins -> Tab available -> Filter Log Parser   
+Check and install without restart   
+  
+Go New Item, Name petclinic-selenium, select Maven-project  
+Hit Ok  
+Source Management   
+Git  https://github.com/<your_repo>/petclinic-test-selenium.git  
+Build  
+Root POM: pom.xml  
+Goals and options: test   
+  
+### Post-build Actions   
+Select Console ouptut (build log) parsing  
+Tick Mark build Failed on Error  
+Tick Use project rule  
+Path to rule file in workspace :  parserules   
+parserules file contains a regex for checking if there is an error 
+
---
+++ src/main/java/petclinic-test.java
@@ -0,0 +1,71 @@
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class petclinic-test {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("chrome");
+        caps.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new
+                URL("http://selenium-hub:4444/wd/hub"), caps);
+        driver.get("http://34.79.12.121:19090/webapp");
+    }
+    @Test
+    public void petclinic() {
+        driver.manage().window().setSize(new Dimension(1920, 1040));
+        driver.findElement(By.cssSelector("li:nth-child(2) span:nth-child(2)")).click();
+        driver.findElement(By.name("lastName")).click();
+        driver.findElement(By.name("lastName")).sendKeys("MEFTAH");
+        driver.findElement(By.cssSelector(".btn:nth-child(1)")).click();
+        driver.findElement(By.linkText("Add Owner")).click();
+        driver.findElement(By.id("firstName")).click();
+        driver.findElement(By.id("firstName")).sendKeys("Herve");
+        driver.findElement(By.id("lastName")).sendKeys("MEFTAH");
+        driver.findElement(By.id("address")).sendKeys("Flat6, 3 Tower mill Road");
+        driver.findElement(By.id("city")).sendKeys("LONDON");
+        driver.findElement(By.id("telephone")).sendKeys("02071215525");
+        driver.findElement(By.cssSelector(".btn")).click();
+        driver.findElement(By.cssSelector("li:nth-child(3) span:nth-child(2)")).click();
+        driver.findElement(By.cssSelector("li:nth-child(2) span:nth-child(2)")).click();
+        driver.findElement(By.cssSelector(".glyphicon-th-list")).click();
+        driver.findElement(By.cssSelector("li:nth-child(1) > a")).click();
+        driver.findElement(By.cssSelector("li:nth-child(4) > a")).click();
+        driver.findElement(By.cssSelector("li:nth-child(3) > a")).click();
+        driver.findElement(By.cssSelector("li:nth-child(1) > a")).click();
+        driver.findElement(By.cssSelector("html")).click();
+        driver.findElement(By.cssSelector("html")).click();
+    }
+
+    @Test
+    public void searchString() {
+        // find search box and enter search string
+        String pagecontent = driver.getPageSource();
+        String search="bonjour";
+        if (pagecontent.toLowerCase().indexOf(search.toLowerCase()) != -1) {
+            System.out.println("OK");
+        }else {
+            System.out.println("Error Webpage content");
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
